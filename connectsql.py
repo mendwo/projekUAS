14c63e5af2a84f5bf2fa72b96cb3812870c0d4fe
Muhammad Rafif <billcipherh@gmail.com>
2025-11-24 16:01:09 +0700

    Add connectsql

---
+++ connectsql.py
@@ -0,0 +1,12 @@
+import psycopg2
+from psycopg2 import Error,sql
+
+def connect():
+    connection = psycopg2.connect(
+    user="postgres",
+    password="123",
+    host = "127.0.0.1",
+    port = "5432",
+    database="Projek 2")
+    cursor = connection.cursor()
+    return cursor
