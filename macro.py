a5aa3fba5419f2da8be7cf7482071b64d4d89cc6
Muhammad Rafif <billcipherh@gmail.com>
2025-11-26 22:08:52 +0700

    macro.py passbintang update for non windows

--- macro.py
+++ macro.py
@@ -11,19 +11,22 @@ def getch_():
 def passbintang(text=""):
     string= ""
     print(text)
-    while True:
-        c = getch()
-        if c[0] > 31 and c[0] < 127:
-            string = string + c.decode('utf-8')
-            print ("*" ,end='',flush=True)
+    if os.name == 'nt':
+        while True:
+            c = getch()
+            if c[0] > 31 and c[0] < 127:
+                string = string + c.decode('utf-8')
+                print ("*" ,end='',flush=True)
 
-        elif c[0] == 8: # backspace , buat hapus
-            clear()
-            print (text)
-            string = string[:len(string)-1]
-            print ("*"*len(string),end='',flush=True)
-        elif c[0] == 13: # enter , buat keluar
-            break
+            elif c[0] == 8: # backspace , buat hapus
+                clear()
+                print (text)
+                string = string[:len(string)-1]
+                print ("*"*len(string),end='',flush=True)
+            elif c[0] == 13: # enter , buat keluar
+                break
+    elif os.name == 'posix':
+        return input()
 
     clear()
     # print(string)
